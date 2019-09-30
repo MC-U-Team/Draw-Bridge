d917fcd116cc3bd6c59e601f0263046bd4c12cd5
HyCraftHD <hycrafthd@live.de>
2019-09-30 02:42:31 +0200

    Use other models particle texture now

--- src/main/java/info/u_team/draw_bridge/model/DrawBridgeModel.java
+++ src/main/java/info/u_team/draw_bridge/model/DrawBridgeModel.java
@@ -6,9 +6,8 @@ import info.u_team.draw_bridge.tileentity.DrawBridgeTileEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.model.*;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IEnviromentBlockReader;
 import net.minecraftforge.api.distmarker.*;
 import net.minecraftforge.client.model.BakedModelWrapper;
 import net.minecraftforge.client.model.data.IModelData;
@@ -21,17 +20,29 @@ public class DrawBridgeModel extends BakedModelWrapper<IBakedModel> {
 	}
 	
 	@Override
-	public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand, IModelData extraData) {
-		if (extraData.hasProperty(DrawBridgeTileEntity.BLOCKSTATE_PROPERTY)) {
-			final BlockState renderBlockState = extraData.getData(DrawBridgeTileEntity.BLOCKSTATE_PROPERTY);
-			return Minecraft.getInstance().getBlockRendererDispatcher().getBlockModelShapes().getModel(renderBlockState).getQuads(renderBlockState, side, rand, extraData);
+	public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand, IModelData data) {
+		final IBakedModel model = getModel(data);
+		if (model != null) {
+			return model.getQuads(state, side, rand, data);
 		}
-		return super.getQuads(state, side, rand, extraData);
+		return super.getQuads(state, side, rand, data);
 	}
 	
 	@Override
-	public IModelData getModelData(IEnviromentBlockReader world, BlockPos pos, BlockState state, IModelData tileData) {
-		return super.getModelData(world, pos, state, tileData);
+	public TextureAtlasSprite getParticleTexture(IModelData data) {
+		final IBakedModel model = getModel(data);
+		if (model != null) {
+			return model.getParticleTexture(data);
+		}
+		return super.getParticleTexture(data);
+	}
+	
+	private IBakedModel getModel(IModelData data) {
+		if (data.hasProperty(DrawBridgeTileEntity.BLOCKSTATE_PROPERTY)) {
+			final BlockState renderBlockState = data.getData(DrawBridgeTileEntity.BLOCKSTATE_PROPERTY);
+			return Minecraft.getInstance().getBlockRendererDispatcher().getBlockModelShapes().getModel(renderBlockState);
+		}
+		return null;
 	}
 	
 }
