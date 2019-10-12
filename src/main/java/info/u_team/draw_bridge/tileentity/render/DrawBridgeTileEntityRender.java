5c218857d0d5acb133f9c4cc0d279f955d1093d2
HyCraftHD <hycrafthd@live.de>
2019-10-12 20:21:43 +0200

    Some refactoring

--- src/main/java/info/u_team/draw_bridge/tileentity/render/DrawBridgeTileEntityRender.java
+++ src/main/java/info/u_team/draw_bridge/tileentity/render/DrawBridgeTileEntityRender.java
@@ -14,23 +14,23 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.data.EmptyModelData;
 
 public class DrawBridgeTileEntityRender extends TileEntityRenderer<DrawBridgeTileEntity> {
 	
-	private BlockRendererDispatcher blockRenderer = Minecraft.getInstance().getBlockRendererDispatcher();
-	
 	@Override
-	public void render(DrawBridgeTileEntity tileEntityIn, double x, double y, double z, float partialTicks, int destroyStage) {
-		BlockPos pos = tileEntityIn.getPos();
-		World world = this.getWorld();
-		Direction facing = tileEntityIn.getFacing();
-		BlockState[] states = tileEntityIn.getBlocksToRender();
-		if (states.length <= 0)
+	public void render(DrawBridgeTileEntity drawBridge, double x, double y, double z, float partialTicks, int destroyStage) {
+		final BlockPos pos = drawBridge.getPos();
+		final World world = this.getWorld();
+		final Direction facing = drawBridge.getFacing();
+		final BlockState[] states = drawBridge.getBlocksToRender();
+		if (states.length <= 0) {
 			return;
-		if ((tileEntityIn.getExtentState() < 10 && tileEntityIn.getExtentState() > 0) || tileEntityIn.isLast()) {
-			Tessellator tessellator = Tessellator.getInstance();
-			BufferBuilder bufferbuilder = tessellator.getBuffer();
-			this.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+		}
+		if ((drawBridge.getExtentState() < 10 && drawBridge.getExtentState() > 0) || drawBridge.isLast()) {
+			final Tessellator tessellator = Tessellator.getInstance();
+			final BufferBuilder bufferBuilder = tessellator.getBuffer();
+			bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
 			RenderHelper.disableStandardItemLighting();
 			GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
 			GlStateManager.enableBlend();
@@ -44,19 +44,19 @@ public class DrawBridgeTileEntityRender extends TileEntityRenderer<DrawBridgeTil
 			GlStateManager.scaled(1.0001, 1.0001, 1.0001);
 			
 			BlockModelRenderer.enableCache();
-			bufferbuilder.begin(7, DefaultVertexFormats.BLOCK);
+			bufferBuilder.begin(7, DefaultVertexFormats.BLOCK);
 			
-			float off = tileEntityIn.getOffset();
-			for (int i = 1; i <= tileEntityIn.getExtentState() + 1; i++) {
-				int of = tileEntityIn.getExtentState() - i + 1;
-				BlockPos tmppos = pos.offset(facing, i);
-				bufferbuilder.setTranslation(x - tmppos.getX() + (facing.getXOffset() * (i - off)), y - tmppos.getY() + (facing.getYOffset() * (i - off)), z - tmppos.getZ() + (facing.getZOffset() * (i - off)));
-				this.renderStateModel(tmppos, states[of], bufferbuilder, world, false);
+			float off = drawBridge.getOffset();
+			for (int i = 1; i <= drawBridge.getExtentState() + 1; i++) {
+				final int of = drawBridge.getExtentState() - i + 1;
+				final BlockPos tmpPos = pos.offset(facing, i);
+				bufferBuilder.setTranslation(x - tmpPos.getX() + (facing.getXOffset() * (i - off)), y - tmpPos.getY() + (facing.getYOffset() * (i - off)), z - tmpPos.getZ() + (facing.getZOffset() * (i - off)));
+				renderStateModel(tmpPos, states[of], bufferBuilder, world, false);
 			}
 			
 			GlStateManager.scaled(1, 1, 1);
 			
-			bufferbuilder.setTranslation(0.0D, 0.0D, 0.0D);
+			bufferBuilder.setTranslation(0.0D, 0.0D, 0.0D);
 			tessellator.draw();
 			BlockModelRenderer.disableCache();
 			RenderHelper.enableStandardItemLighting();
@@ -64,15 +64,13 @@ public class DrawBridgeTileEntityRender extends TileEntityRenderer<DrawBridgeTil
 	}
 	
 	@Override
-	public boolean isGlobalRenderer(DrawBridgeTileEntity te) {
+	public boolean isGlobalRenderer(DrawBridgeTileEntity drawBridge) {
 		return true;
 	}
 	
-	@SuppressWarnings("deprecation")
 	private boolean renderStateModel(BlockPos pos, BlockState state, BufferBuilder buffer, World world, boolean checkSides) {
-		if (blockRenderer == null)
-			blockRenderer = Minecraft.getInstance().getBlockRendererDispatcher();
-		return this.blockRenderer.getBlockModelRenderer().renderModel(world, this.blockRenderer.getModelForState(state), state, pos, buffer, checkSides, new Random(), state.getPositionRandom(pos));
+		final BlockRendererDispatcher blockRenderer = Minecraft.getInstance().getBlockRendererDispatcher();
+		return blockRenderer.getBlockModelRenderer().renderModel(world, blockRenderer.getModelForState(state), state, pos, buffer, checkSides, new Random(), state.getPositionRandom(pos), EmptyModelData.INSTANCE);
 	}
 	
 }
