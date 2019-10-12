d3553bc2fb82d1cac3b6a37620c1e032cc212d45
HyCraftHD <hycrafthd@live.de>
2019-10-12 20:17:52 +0200

    Change registry of tile entity special render to an own class

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeRenders.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeRenders.java
@@ -1,8 +1,9 @@
 package info.u_team.draw_bridge.init;
 
 import info.u_team.draw_bridge.DrawBridgeMod;
-import info.u_team.draw_bridge.screen.DrawBridgeScreen;
-import net.minecraft.client.gui.ScreenManager;
+import info.u_team.draw_bridge.tileentity.DrawBridgeTileEntity;
+import info.u_team.draw_bridge.tileentity.render.DrawBridgeTileEntityRender;
+import info.u_team.u_team_core.util.registry.ClientRegistry;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -10,11 +11,11 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @EventBusSubscriber(modid = DrawBridgeMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
-public class DrawBridgeGuis {
+public class DrawBridgeRenders {
 	
 	@SubscribeEvent
 	public static void register(FMLClientSetupEvent event) {
-		ScreenManager.registerFactory(DrawBridgeContainerTypes.DRAW_BRIDGE, DrawBridgeScreen::new);
+		ClientRegistry.registerSpecialTileEntityRenderer(DrawBridgeTileEntity.class, new DrawBridgeTileEntityRender());
 	}
 	
 }
