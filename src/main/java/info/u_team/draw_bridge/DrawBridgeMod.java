f329df2c9d440c275d1e62b36d543b72d7a2504d
HyCraftHD <hycrafthd@live.de>
2020-02-10 16:17:38 +0100

    Add signer check to mod

--- src/main/java/info/u_team/draw_bridge/DrawBridgeMod.java
+++ src/main/java/info/u_team/draw_bridge/DrawBridgeMod.java
@@ -1,10 +1,15 @@
 package info.u_team.draw_bridge;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(DrawBridgeMod.MODID)
 public class DrawBridgeMod {
 	
 	public static final String MODID = "drawbridge";
+	
+	public DrawBridgeMod() {
+		JarSignVerifier.checkSigned(MODID);
+	}
 	
 }
