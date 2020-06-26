d97be03d2e7d4fb4dd378a7e6b79bc7d4d32f5fd
HyCraftHD <hycrafthd@live.de>
2020-06-26 23:44:33 +0200

    Use the deferred register for the draw bridge tile entity type

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeTileEntityTypes.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeTileEntityTypes.java
@@ -2,21 +2,16 @@ package info.u_team.draw_bridge.init;
 
 import info.u_team.draw_bridge.DrawBridgeMod;
 import info.u_team.draw_bridge.tileentity.DrawBridgeTileEntity;
-import info.u_team.u_team_core.tileentitytype.UTileEntityType.UBuilder;
-import info.u_team.u_team_core.util.registry.*;
-import net.minecraft.inventory.container.ContainerType;
+import info.u_team.u_team_core.util.registry.TileEntityTypeDeferredRegister;
 import net.minecraft.tileentity.TileEntityType;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.*;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 
 public class DrawBridgeTileEntityTypes {
 	
 	public static final TileEntityTypeDeferredRegister TILE_ENTITY_TYPES = TileEntityTypeDeferredRegister.create(DrawBridgeMod.MODID);
 	
-	public static final TileEntityType<DrawBridgeTileEntity> DRAW_BRIDGE = UBuilder.create("draw_bridge", DrawBridgeTileEntity::new, DrawBridgeBlocks.DRAW_BRIDGE).build();
+	public static final RegistryObject<TileEntityType<DrawBridgeTileEntity>> DRAW_BRIDGE = TILE_ENTITY_TYPES.register("draw_bridge", () -> TileEntityType.Builder.create(DrawBridgeTileEntity::new, DrawBridgeBlocks.DRAW_BRIDGE.get()));
 	
 	public static void register(IEventBus bus) {
 		TILE_ENTITY_TYPES.register(bus);
