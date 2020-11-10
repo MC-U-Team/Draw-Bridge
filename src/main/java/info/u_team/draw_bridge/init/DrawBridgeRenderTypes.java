d552d2f4b05459ddf15ca00ab54ccf8d5aef0044
HyCraftHD <hycrafthd@live.de>
2020-11-10 18:25:47 +0100

    Change render types

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeRenderTypes.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeRenderTypes.java
@@ -1,22 +1,20 @@
 package info.u_team.draw_bridge.init;
 
-import info.u_team.draw_bridge.DrawBridgeMod;
 import net.minecraft.client.renderer.*;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = DrawBridgeMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class DrawBridgeRenderTypes {
 	
-	@SubscribeEvent
-	public static void register(FMLClientSetupEvent event) {
+	private static void setup(FMLClientSetupEvent event) {
 		// Translucent
 		final RenderType translucent = RenderType.getTranslucent();
 		
 		RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE.get(), translucent);
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(DrawBridgeRenderTypes::setup);
 	}
 	
 }
