e55cb1e96ff541049a936cd3fb8b5641e5e2f990
HyCraftHD <hycrafthd@live.de>
2020-11-10 18:20:52 +0100

    Add common register stuff

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeCommonConstruct.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeCommonConstruct.java
@@ -2,12 +2,16 @@ package info.u_team.draw_bridge.init;
 
 import info.u_team.draw_bridge.DrawBridgeMod;
 import info.u_team.u_team_core.api.construct.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
 
 @Construct(modid = DrawBridgeMod.MODID)
 public class DrawBridgeCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		BusRegister.registerMod(DrawBridgeBlocks::register);
+		BusRegister.registerMod(DrawBridgeContainerTypes::register);
+		BusRegister.registerMod(DrawBridgeTileEntityTypes::register);
 	}
 	
 }
