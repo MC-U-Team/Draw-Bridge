49f41b86012f26aa17104eef7fb184b37ddcd28d
HyCraftHD <hycrafthd@live.de>
2020-11-10 18:20:10 +0100

    Add Drawbridge common construct

---
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeCommonConstruct.java
@@ -0,0 +1,13 @@
+package info.u_team.draw_bridge.init;
+
+import info.u_team.draw_bridge.DrawBridgeMod;
+import info.u_team.u_team_core.api.construct.*;
+
+@Construct(modid = DrawBridgeMod.MODID)
+public class DrawBridgeCommonConstruct implements IModConstruct {
+	
+	@Override
+	public void construct() {
+	}
+	
+}
