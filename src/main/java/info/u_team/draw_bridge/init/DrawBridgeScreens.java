f6527498fd9a9ca7bd2da5c90b765823d5a5ead1
HyCraftHD <hycrafthd@live.de>
2020-11-10 18:26:34 +0100

    Update screens

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeScreens.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeScreens.java
@@ -1,22 +1,20 @@
 package info.u_team.draw_bridge.init;
 
-import info.u_team.draw_bridge.DrawBridgeMod;
 import info.u_team.draw_bridge.screen.DrawBridgeScreen;
-import info.u_team.u_team_core.util.registry.*;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import info.u_team.u_team_core.util.registry.ClientRegistry;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = DrawBridgeMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class DrawBridgeScreens {
 	
-	@SubscribeEvent
-	public static void register(FMLClientSetupEvent event) {
+	private static void setup(FMLClientSetupEvent event) {
 		MainThreadWorker.run(() -> {
 			ClientRegistry.registerScreen(DrawBridgeContainerTypes.DRAW_BRIDGE, DrawBridgeScreen::new);
 		});
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(DrawBridgeScreens::setup);
 	}
 	
 }
