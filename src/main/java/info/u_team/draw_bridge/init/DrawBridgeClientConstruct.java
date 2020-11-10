988d729a7522b7e05fa2cd56e1c4c4f42d21c6f8
HyCraftHD <hycrafthd@live.de>
2020-11-10 18:24:23 +0100

    Register draw bridge models

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeClientConstruct.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeClientConstruct.java
@@ -2,12 +2,14 @@ package info.u_team.draw_bridge.init;
 
 import info.u_team.draw_bridge.DrawBridgeMod;
 import info.u_team.u_team_core.api.construct.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
 
 @Construct(modid = DrawBridgeMod.MODID, client = true)
 public class DrawBridgeClientConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		BusRegister.registerMod(DrawBridgeModels::registerMod);
 	}
 	
 }
