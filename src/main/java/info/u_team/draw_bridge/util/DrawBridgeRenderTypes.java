994e55a8517bbbf2c17572da525fdea4444a0607
HyCraftHD <hycrafthd@live.de>
2020-11-12 02:24:59 +0100

    Add util enum for render types for drawbridge

---
+++ src/main/java/info/u_team/draw_bridge/util/DrawBridgeRenderTypes.java
@@ -0,0 +1,27 @@
+package info.u_team.draw_bridge.util;
+
+import static info.u_team.draw_bridge.init.DrawBridgeBlocks.*;
+
+import java.util.function.Supplier;
+
+import info.u_team.draw_bridge.block.DrawBridgeBlock;
+
+public enum DrawBridgeRenderTypes {
+	
+	SOLID(DRAW_BRIDGE),
+	CUTOUT(DRAW_BRIDGE_CUTOUT),
+	CUTOUT_MIPPED(DRAW_BRIDGE_CUTOUT_MIPPED),
+	TRANSLUCTENT(DRAW_BRIDGE_TRANSLUCTENT),
+	TRIPWIRE(DRAW_BRIDGE_TRIPWIRE);
+	
+	private final Supplier<? extends DrawBridgeBlock> blockSupplier;
+	
+	private DrawBridgeRenderTypes(Supplier<? extends DrawBridgeBlock> blockSupplier) {
+		this.blockSupplier = blockSupplier;
+	}
+	
+	public DrawBridgeBlock getBlock() {
+		return blockSupplier.get();
+	}
+	
+}
