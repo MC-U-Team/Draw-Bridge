38a292abe827177ec6babe43438da834cca90747
HyCraftHD <hycrafthd@live.de>
2020-11-12 01:52:31 +0100

    Register render types

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeRenderTypes.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeRenderTypes.java
@@ -1,15 +1,17 @@
 package info.u_team.draw_bridge.init;
 
+import net.minecraft.client.renderer.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class DrawBridgeRenderTypes {
 	
 	private static void setup(FMLClientSetupEvent event) {
-		// Translucent
-		// final RenderType translucent = RenderType.getTranslucent();
-		//
-		// RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE.get(), RenderType.getCutout());
+		RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE_SOLID.get(), RenderType.getSolid());
+		RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE_CUTOUT.get(), RenderType.getCutout());
+		RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE_CUTOUT_MIPPED.get(), RenderType.getCutoutMipped());
+		RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE_TRANSLUCTENT.get(), RenderType.getTranslucent());
+		RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE_TRIPWIRE.get(), RenderType.getTripwire());
 	}
 	
 	public static void registerMod(IEventBus bus) {
