d27039085ee316591bb3c4afccbd2a247ae76dd0
HyCraftHD <hycrafthd@live.de>
2020-11-12 02:07:15 +0100

    Should generate models for blocks

--- src/data/java/info/u_team/draw_bridge/data/provider/DrawBridgeBlockStatesProvider.java
+++ src/data/java/info/u_team/draw_bridge/data/provider/DrawBridgeBlockStatesProvider.java
@@ -1,7 +1,10 @@
 package info.u_team.draw_bridge.data.provider;
 
-import static info.u_team.draw_bridge.init.DrawBridgeBlocks.DRAW_BRIDGE;
+import static info.u_team.draw_bridge.init.DrawBridgeBlocks.*;
 
+import com.google.common.collect.ImmutableList;
+
+import info.u_team.draw_bridge.block.DrawBridgeBlock;
 import info.u_team.u_team_core.data.*;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.*;
@@ -27,6 +30,16 @@ public class DrawBridgeBlockStatesProvider extends CommonBlockStatesProvider {
 					.rotationY(direction.getAxis().isVertical() ? 0 : (((int) direction.getHorizontalAngle()) + 180) % 360) //
 					.build(); //
 		});
+		
+		for (DrawBridgeBlock renderBlocks : ImmutableList.of(DRAW_BRIDGE_SOLID.get(), DRAW_BRIDGE_CUTOUT.get(), DRAW_BRIDGE_CUTOUT_MIPPED.get(), DRAW_BRIDGE_TRANSLUCTENT.get(), DRAW_BRIDGE_TRIPWIRE.get())) {
+			getVariantBuilder(renderBlocks).forAllStates(state -> {
+				final Direction direction = state.get(BlockStateProperties.FACING);
+				return ConfiguredModel.builder() //
+						.modelFile(models().getExistingFile(modLoc("draw_bridge"))) //
+						.rotationX(direction == Direction.DOWN ? 90 : direction == Direction.UP ? 270 : 0) //
+						.rotationY(direction.getAxis().isVertical() ? 0 : (((int) direction.getHorizontalAngle()) + 180) % 360) //
+						.build(); //
+			});
+		}
 	}
-	
 }
