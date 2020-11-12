10d6ca5905eea392b5b9b3b93b306e6df03b9782
HyCraftHD <hycrafthd@live.de>
2020-11-12 01:27:44 +0100

    Comment out render types for now

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeRenderTypes.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeRenderTypes.java
@@ -1,6 +1,5 @@
 package info.u_team.draw_bridge.init;
 
-import net.minecraft.client.renderer.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -8,9 +7,9 @@ public class DrawBridgeRenderTypes {
 	
 	private static void setup(FMLClientSetupEvent event) {
 		// Translucent
-		final RenderType translucent = RenderType.getTranslucent();
-		
-		RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE.get(), translucent);
+		// final RenderType translucent = RenderType.getTranslucent();
+		//
+		// RenderTypeLookup.setRenderLayer(DrawBridgeBlocks.DRAW_BRIDGE.get(), RenderType.getCutout());
 	}
 	
 	public static void registerMod(IEventBus bus) {
