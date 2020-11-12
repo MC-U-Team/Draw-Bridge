122260a562af945fef70025537f9302d1a7a758f
HyCraftHD <hycrafthd@live.de>
2020-11-12 03:09:01 +0100

    Add cycle method

--- src/main/java/info/u_team/draw_bridge/util/DrawBridgeCamouflageRenderTypes.java
+++ src/main/java/info/u_team/draw_bridge/util/DrawBridgeCamouflageRenderTypes.java
@@ -2,7 +2,10 @@ package info.u_team.draw_bridge.util;
 
 import static info.u_team.draw_bridge.init.DrawBridgeBlocks.*;
 
+import java.util.List;
 import java.util.function.Supplier;
+
+import com.google.common.collect.ImmutableList;
 
 import info.u_team.draw_bridge.block.DrawBridgeBlock;
 
@@ -14,6 +17,8 @@ public enum DrawBridgeCamouflageRenderTypes {
 	TRANSLUCTENT(DRAW_BRIDGE_TRANSLUCTENT),
 	TRIPWIRE(DRAW_BRIDGE_TRIPWIRE);
 	
+	public static final List<DrawBridgeCamouflageRenderTypes> RENDER_TYPES = ImmutableList.copyOf(values());
+	
 	private final Supplier<? extends DrawBridgeBlock> blockSupplier;
 	
 	private DrawBridgeCamouflageRenderTypes(Supplier<? extends DrawBridgeBlock> blockSupplier) {
@@ -22,6 +27,23 @@ public enum DrawBridgeCamouflageRenderTypes {
 	
 	public DrawBridgeBlock getBlock() {
 		return blockSupplier.get();
+	}
+	
+	public static DrawBridgeCamouflageRenderTypes cycle(DrawBridgeCamouflageRenderTypes type) {
+		switch (type) {
+		case SOLID:
+			return CUTOUT;
+		case CUTOUT:
+			return CUTOUT_MIPPED;
+		case CUTOUT_MIPPED:
+			return TRANSLUCTENT;
+		case TRANSLUCTENT:
+			return TRIPWIRE;
+		case TRIPWIRE:
+			return SOLID;
+		default:
+			return SOLID;
+		}
 	}
 	
 }
