40a086b58726695675a317c2014ff1c6a41dec0a
HyCraftHD <hycrafthd@live.de>
2020-11-12 01:52:26 +0100

    Register blocks

--- src/main/java/info/u_team/draw_bridge/init/DrawBridgeBlocks.java
+++ src/main/java/info/u_team/draw_bridge/init/DrawBridgeBlocks.java
@@ -1,7 +1,7 @@
 package info.u_team.draw_bridge.init;
 
 import info.u_team.draw_bridge.DrawBridgeMod;
-import info.u_team.draw_bridge.block.DrawBridgeBlock;
+import info.u_team.draw_bridge.block.*;
 import info.u_team.u_team_core.util.registry.*;
 import net.minecraft.item.BlockItem;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,6 +11,12 @@ public class DrawBridgeBlocks {
 	public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(DrawBridgeMod.MODID);
 	
 	public static final BlockRegistryObject<DrawBridgeBlock, BlockItem> DRAW_BRIDGE = BLOCKS.register("draw_bridge", DrawBridgeBlock::new);
+	
+	public static final BlockRegistryObject<DrawBridgeBlock, BlockItem> DRAW_BRIDGE_SOLID = BLOCKS.register("draw_bridge_solid", DrawBridgeCamouflage::new);
+	public static final BlockRegistryObject<DrawBridgeBlock, BlockItem> DRAW_BRIDGE_CUTOUT = BLOCKS.register("draw_bridge_cutout", DrawBridgeCamouflage::new);
+	public static final BlockRegistryObject<DrawBridgeBlock, BlockItem> DRAW_BRIDGE_CUTOUT_MIPPED = BLOCKS.register("draw_bridge_cutout_mipped", DrawBridgeCamouflage::new);
+	public static final BlockRegistryObject<DrawBridgeBlock, BlockItem> DRAW_BRIDGE_TRANSLUCTENT = BLOCKS.register("draw_bridge_translucent", DrawBridgeCamouflage::new);
+	public static final BlockRegistryObject<DrawBridgeBlock, BlockItem> DRAW_BRIDGE_TRIPWIRE = BLOCKS.register("draw_bridge_tripwire", DrawBridgeCamouflage::new);
 	
 	public static void registerMod(IEventBus bus) {
 		BLOCKS.register(bus);
